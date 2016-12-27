9d8644b2d580d504e7f1884c0b4973df151a23df
Andrey Kraveckiy <andreykraveckiy@gmail.com>
2016-12-27 13:27:10 +0200

    Change form to lonk. Decrease response method

--- lib/racker.rb
+++ lib/racker.rb
@@ -14,42 +14,70 @@ class Racker
   def response
     case @request.path
     when '/' then Rack::Response.new(render('index.html.erb'))
-    when '/new_game' 
-      @game.listens_and_shows("new game")
-      Rack::Response.new(render('index.html.erb'))
-    when '/scores' 
-      @game.listens_and_shows("scores")
-      Rack::Response.new(render('index.html.erb'))
-    when '/back'
-      @game.listens_and_shows("back")
-      Rack::Response.new(render('index.html.erb'))
-    when '/hint'
-      @what_ask = "Hint"
-      @game.listens_and_shows("hint")
-      Rack::Response.new(render('index.html.erb'))
-    when '/guess'
-      @what_ask = @request.params['guess']
-      @game.listens_and_shows(@what_ask) 
-      Rack::Response.new(render('index.html.erb'))
-    when '/restart'
-      @game.listens_and_shows('restart')
-      Rack::Response.new(render('index.html.erb'))
-    when '/yes'
-      @game.listens_and_shows('yes')
-      Rack::Response.new(render('index.html.erb'))
-    when '/no'
-      @game.listens_and_shows('no')
-      @request.session.clear
-      redirect_to_root
-    when '/save'
-      @game.listens_and_shows(@request.params['user_name'])
-      Rack::Response.new(render('index.html.erb'))
+    when '/new_game' then new_game_path
+    when '/scores' then scores_path
+    when '/back' then back_path
+    when '/hint' then hint_path
+    when '/guess' then guess_path
+    when '/restart' then restart_path
+    when '/yes' then yes_path
+    when '/no' then no_path
+    when '/save' then save_path
     else
       Rack::Response.new('Not Found', 404)
     end
   end
 
   protected
+    def new_game_path
+      @game.listens_and_shows("new game")
+      Rack::Response.new(render('index.html.erb'))
+    end
+
+    def scores_path
+      @game.listens_and_shows("scores")
+      Rack::Response.new(render('index.html.erb'))
+    end
+
+    def back_path
+      @game.listens_and_shows("back")
+      Rack::Response.new(render('index.html.erb'))
+    end
+
+    def hint_path
+      @what_ask = "Hint"
+      @game.listens_and_shows("hint")
+      Rack::Response.new(render('index.html.erb'))
+    end
+
+    def guess_path
+      @what_ask = @request.params['guess']
+      @game.listens_and_shows(@what_ask) 
+      Rack::Response.new(render('index.html.erb'))
+    end
+
+    def restart_path
+      @game.listens_and_shows('restart')
+      Rack::Response.new(render('index.html.erb'))
+    end
+
+    def yes_path
+      @game.listens_and_shows('yes')
+      Rack::Response.new(render('index.html.erb'))
+    end
+
+    def no_path
+      @game.listens_and_shows('no')
+      @request.session.clear
+      redirect_to_root
+    end
+
+    def save_path     
+      name =  @request.params['user_name']
+      @game.listens_and_shows(name) unless name.empty?
+      Rack::Response.new(render('index.html.erb'))
+    end
+
     def render(template)
       @partial = '_' + @game.stage.to_s + '_stage.html.erb'
       path = File.expand_path("../views/#{template}", __FILE__)
